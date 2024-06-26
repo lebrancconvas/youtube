1a32913c924df82db5dcadf5000bb1a04e872de8
paranoidbarbie <brown.hannah2024@gmail.com>
2024-06-26 15:58:46 +0100

    Chatapp done

--- python/chatapp/client.py
+++ python/chatapp/client.py
@@ -1,31 +1,34 @@
-import tkinter as tk 
+import tkinter as tk
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
-class Client():
-    def __init__(self,window, host, port):
 
+class Client():
+    def __init__(self, window, host, port):
         self.window = window
 
-        self.messageBox = tk.Listbox(window, height=20, width=100, bg="#e75480")
+        self.messageBox = tk.Listbox(window, height=20, width=100, bg="#fc6c85")
         self.messageBox.pack()
 
-        self.msgEntry = tk.Entry(window, width=100, bg="#f5f5dc", fg="#0F0F0F")
+        self.msgEntry = tk.Entry(window, width=100, bg="#fc6c85", fg="#000000")
         self.msgEntry.pack()
 
-        self.button = tk.Button(window, text="Send", width=50,bg="#E75480", command=self.sendTexts)
+        self.button = tk.Button(window, text="Send", width=50, bg="#fc6c85", command=self.sendTexts)
         self.button.pack()
 
 
+        #define hosts and port
         self.host = host
         self.port = port
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.buffer = 1024
         self.sock.connect((self.host, self.port))
 
-        Thread(target=self.recvTexts).start()
+        Thread(target=self.recvTexts).start() 
 
-    
+        #we don't need extra functin since we won't apply any extra functionality.
+
+
     def sendTexts(self):
         self.sock.send(bytes(self.msgEntry.get(), "utf8"))
 
@@ -33,18 +36,17 @@ class Client():
         while True:
             try:
                 msg = self.sock.recv(self.buffer).decode("utf8")
-                #print(msg)
                 self.messageBox.insert(tk.END, msg)
             except OSError:
                 break
 
 
+
 if __name__ == "__main__":
     window = tk.Tk()
-    window.title("Chat app")
+    window.title("Simplest gui chatapp")
     Client(window, "localhost", 8000)
-    try:
+    try: 
         window.mainloop()
     except KeyboardInterrupt:
         window.destroy()
-
--- python/chatapp/server.py
+++ python/chatapp/server.py
@@ -1,5 +1,7 @@
 from socket import AF_INET, socket, SOCK_STREAM
-from threading import Thread 
+from threading import Thread
+
+
 
 class Server():
     def __init__(self, host, port):
@@ -7,49 +9,42 @@ class Server():
         self.port = port
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.buffer = 1024
-        self.clients = {}
-        self.addrs = {}
+        self.clients = {} # to have multiple clients and list them here
+        self.addrs = {} # same as above
 
-    
+    #starting the server and accepting the clients
     def serverStart(self):
-        try:
-            self.sock.bind((self.host, self.port))
-            self.sock.listen(5)
-            print("We're connected master >_< -w-")
+        self.sock.bind((self.host, self.port))
+        self.sock.listen(5)
 
-        except OSError:
-            print("Please close previous connection first.")
-    
-        while True: #this recieves clients
+        print("We're ready master >_< ")
+
+        while True:
             try:
                 cliSock, cliAddr = self.sock.accept()
                 print(f"Connection from {cliAddr}")
 
-        #add the client details in the arrays
-
-                self.clients[cliSock] = cliAddr
+                self.clients[cliSock] = cliAddr # adding all the clients to self.clients arrays
                 self.addrs[cliAddr] = cliSock
-
-        #start a thread to keep recieving clients
 
                 Thread(target=self.handling, args=(cliSock,)).start()
             except Exception as e:
                 print(e)
-    
+
+
+    #function to receieve texts and display them in the terminal
     def handling(self, cliSock):
         while True:
             try:
                 msg = cliSock.recv(self.buffer).decode("utf8")
+                ip, port = self.clients[cliSock] 
                 if msg:
-                    ip, port = self.clients[cliSock]
-                    print(f"{msg} Recived from {port}")
-                    self.sendTexts(msg)
+                    print(f"{msg} recieved from {port}")
+                    self.sendText(msg)
             except Exception as e:
                 print(e)
 
-    
-
-    def sendTexts(self, msg):
+    def sendText(self, msg):
         for socket in self.clients:
             try:
                 socket.send(bytes(msg, "utf8"))
@@ -57,11 +52,8 @@ class Server():
                 print(e)
 
 
-
-
 if __name__ == "__main__":
     try:
         Server("localhost", 8000).serverStart()
     except KeyboardInterrupt:
-        print("Connection closed")
-
+        print("Closed Connection.")
