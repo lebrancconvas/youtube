72b9af8af04b6f3d29dea21f85ae3c0c0553d715
paranoidbarbie <brown.hannah2024@gmail.com>
2024-06-30 15:19:46 +0100

    AI colour palette done

--- python/colourPalette/colour.py
+++ python/colourPalette/colour.py
@@ -1,18 +1,18 @@
-from numpy import array
+from numpy import array, reshape
 from sklearn.cluster import KMeans
 
 
 class ColourGeneration():
-
-    def generate(self, image, colourIndex):
+    def generateColours(self, image, rangeVal):
         colourArray = array(image)
         reshape = colourArray.reshape(-1, 3)
-        kmeans = KMeans(n_clusters=colourIndex)
+        kmeans = KMeans(n_clusters=rangeVal)
         kmeans.fit(reshape)
         colours = kmeans.cluster_centers_
         colours = colours.astype(int)
         return colours
 
-
 if __name__ == "__main__":
     ColourGeneration()
+
+
--- python/colourPalette/main.py
+++ python/colourPalette/main.py
@@ -1,129 +1,70 @@
-import tkinter as tk
-from tkinter import Canvas, Button, Frame, Label, OptionMenu, StringVar, filedialog
+import tkinter as tk 
+from tkinter import Label, Frame, Canvas, Button, Frame, OptionMenu, StringVar, filedialog
 from PIL import Image, ImageTk
-from colour import ColourGeneration 
+
+from colour import ColourGeneration
+
+
 
 class Palette():
     def __init__(self, window):
         self.window = window
-        
-        self.canvas = Canvas(self.window, width=320, height=180)
+
+        #displays the image in this box
+        self.canvas = Canvas(self.window, width=320, height=180) #it's a 16:9 ratio
         self.canvas.pack(padx=10)
 
-        self.button = Button(self.window, text="Select Image", command=self.getPath)
-        self.button.pack(pady=10, padx=10)
+        #button
+        self.button = Button(self.window, text="Select", command=self.getPath)
+        self.button.pack(padx=10, pady=10)
         
+        #options for colours
         self.option = StringVar(value="1")
         self.select = OptionMenu(self.window, self.option, "1", "2", "3", "4", "5")
         self.select.pack(padx=10, pady=10)
-
+    
+        #frame which will hold the colours
         self.frame = Frame(self.window)
         self.frame.pack(padx=10)
-        
+
+
     def getPath(self):
         fileType = [(".jpg", ".png")]
         path = filedialog.askopenfilename(filetypes=fileType)
         if path:
             self.displayImage(path)
 
+
     def displayImage(self, path):
         image = Image.open(path).resize((320, 180))
         photo = ImageTk.PhotoImage(image)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=photo)
         self.canvas.image = photo
-        self.getColours(image)
-    
+        self.getColours(image) #just use the resized image not the photoimage       
+
     def getColours(self, photo):
         rangeVal = int(self.option.get())
-        colours = ColourGeneration().generate(photo, rangeVal)
+        colours = ColourGeneration().generateColours(photo, rangeVal)
         palette = []
-        for colour in colours: 
+        for colour in colours:
             colour = palette.append(f"#{colour[0]:02x}{colour[1]:02x}{colour[2]:02x}")
-            self.generatePalette(palette) 
+            self.generatePalette(palette)
     
     def generatePalette(self, colour):
-        for index, color in enumerate(colour):
-            label = Label(self.frame, bg=color, width=10, height=10)
+        for index, colour in enumerate(colour):
+            label = Label(self.frame, bg=colour, width=10, height=10)
             label.grid(row=0, column=index, padx=10, pady=10)
-        
-        
 
 
+        
 if __name__ == "__main__":
     window = tk.Tk()
-    window.title = "Colour Palette"
+    window.title = "AI colour palette"
     app = Palette(window)
     try:
         window.mainloop()
     except Exception as e:
         print(e)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
