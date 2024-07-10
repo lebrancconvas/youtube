48d091d0d5029a1fcbae21922f1ef649aa9a1e61
paranoidbarbie <brown.hannah2024@gmail.com>
2024-07-10 10:40:04 +0100

    Palindrome Checker

---
+++ rust/README.md
@@ -0,0 +1,4 @@
+# All video guides
+
+## Palindrome Chcker
+[![Palindrome Checker](https://img.youtube.com/vi/kXk6uTwjtr8/sddefault.jpg)](https://www.youtube.com/watch?v=kXk6uTwjtr8)
--- rust/palindrome/src/main.rs
+++ rust/palindrome/src/main.rs
@@ -1,28 +1,60 @@
-use std::io;
-use std::io::Write;
+// here we'll have all the files, libraries, modules etc.
 
-fn is_palindrome(s: &str) -> bool {
-    // Convert the input string to lowercase and filter out non-alphanumeric characters
-    let sanitize: String = s.chars().filter(|c| c.is_ascii_alphanumeric()).collect::<String>().to_lowercase();
+//import libraries 
+//
+use std::io::{self, Write};
 
-    // Check if sanitized line reads the same forwards and backwards 
-    // it is not a if else statement
-    sanitize == sanitize.chars().rev().collect::<String>()
+// let's define the function which will check for the palindrome
+//s will refer to a str which will return a boolean value 
+fn palindrome(s: &str) -> bool {
+    // sanitize the variable s with alphanumerics
+    // chars iterates over all the characters and filter removes all the commas and periods,
+    // collect then joins all the characters and returns a string 
+    let sanitize: String = s.
+        chars().
+        filter(|c| c.is_ascii_alphanumeric())
+        .collect::<String>()
+        .to_lowercase();
+    // compare to the reverse value of every char and collect it into a string.
+
+    sanitize == sanitize
+        .chars()
+        .rev()
+        .collect::<String>()
 }
+
+//create function like this
 
 fn main() {
-    // Taking user input use println for new line
-    print!("Type a word or sentence -> ");
-    //print as we type
+    //but the prompt is on a new line , let's fix that
+    print!("Type a word -> ");
     io::stdout().flush().unwrap();
+    // take user input with the io library 
+    // mut means mutable. generally a variable is immutable.
+    // define a variable which will store this input
     let mut word = String::new();
-    io::stdin().read_line(&mut word).expect("Failed to read line.");
-    let word = word.trim();
-    /* Pint nothing or not depening on the output
-    if is_palindrome(word) {
-        println!("{word} is a palindrome")
+    io::stdin().read_line(&mut word).expect("Failed to read.");
+    let word = word.trim(); //trim the whitespace after taking the input
+    //print the variable
+    // the new line there because of rust's feature
+    // we'll see how to remove that in future.
+    if palindrome(word) {
+        println!("{word} -> is A palindrome.");
     } else {
-        println!("{word} is not a palindrome")
-    }*/
-    println!("\"{word}\" is{} a palindrome.", if is_palindrome(word) { "" } else { " not" });
+        println!("{word} -> is not a palindrome.");
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
