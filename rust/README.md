48300b821877a895a7497acb0acecdf7d3ccbc70
paranoidbarbie <brown.hannah2024@gmail.com>
2024-07-12 14:48:12 +0100

    Word finder

--- rust/README.md
+++ rust/README.md
@@ -2,3 +2,6 @@
 
 ## Palindrome Chcker
 [![Palindrome Checker](https://img.youtube.com/vi/kXk6uTwjtr8/sddefault.jpg)](https://www.youtube.com/watch?v=kXk6uTwjtr8)
+
+## Word Finder 
+[![Word Finder](https://img.youtube.com/vi/IiCrkXUMXr0/sddefault.jpg)](https://www.youtube.com/watch?v=IiCrkXUMXr0)
--- rust/wordfinder/src/main.rs
+++ rust/wordfinder/src/main.rs
@@ -1,51 +1,53 @@
+//import the file reader crate
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+//let's implement the file reading function
+
 fn word_finder(file_path: &str, target_word: &str) -> io::Result<()> {
     let path = Path::new(file_path);
-    // Open the file
-    let file = File::open(&path)?;
-    let reader = io::BufReader::new(file);
 
+    let file = File::open(&path)?;
+    //let's make a vector which will hold all the words.
+    let reader = io::BufReader::new(file);
     let mut number_of_words = Vec::new();
 
-    let mut line_num: i32 = 0;
-    // Iterate over lines in the file
+    // just in each iteration, increment the number and print on each occurances.
+
+    let mut line_num: i32 = 0; // we can use u32 also. i means signed and u is unsigned
+
+    //iterate over all the lines
     for line in reader.lines() {
-        //dbg!(&line);
         line_num += 1;
         let line = line?;
 
-        let mut col_num: i32 = 0;
-        // Split the line into words and check each word
+        let mut col_num: i32 = 0; //same with col also
         for word in line.split_whitespace() {
-            dbg!(&word);
+            //remove the alphanumerics
             let word: String = word
                 .chars()
                 .filter(|c| c.is_ascii_alphanumeric())
                 .collect::<String>()
                 .to_lowercase();
 
-            //dbg!(line.split_whitespace());
             col_num += 1;
             if word == target_word {
-                number_of_words.push((line_num, col_num, word.to_string()));
-                //println!("Found word: {} {}:{}", word, line_num, col_num);
+                //push the words in the vector.
+                number_of_words.push((line_num, col_num, word.to_string())); 
             }
         }
     }
-    dbg!(&number_of_words);
+    //iterating over the contents of the vector
     for (line_num, col_num, word) in number_of_words {
-        println!("{} {}:{}", word, line_num, col_num);
+        println!("{} -> at {}:{}", word, line_num, col_num);
     }
     Ok(())
 }
 
 fn main() {
-    //println!("Current working directory: {:?}", std::env::current_dir());
-    let file_path = "test.txt"; //file path
-    let target_word = "example"; // word to find
+    let file_path = "test.txt";
+    let target_word = "okay";
 
     if let Err(err) = word_finder(file_path, target_word) {
         eprintln!("Error searching for word: {}", err);
